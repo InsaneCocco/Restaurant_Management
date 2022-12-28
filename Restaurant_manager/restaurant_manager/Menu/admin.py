6b504ece1228b217d2929ce042fa828ff3f8209b
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2022-12-28 14:16:49 -0600

    fixed theargument of the admin file and migrated the models

--- Restaurant_manager/restaurant_manager/Menu/admin.py
+++ Restaurant_manager/restaurant_manager/Menu/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import Menu_items
 
 # Register your models here.
-admin.site.register('Menu_items')
+admin.site.register(Menu_items)
--- Restaurant_manager/restaurant_manager/Menu/models.py
+++ Restaurant_manager/restaurant_manager/Menu/models.py
@@ -2,5 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Menu_items(models.Model):
-    dish_name(models.CharField(max_length=30))
-    
+    dish_name = (models.CharField(max_length=30))
