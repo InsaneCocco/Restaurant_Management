74cbb8903cf02e8097cd419411e7a8ea46727af6
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2022-12-29 17:41:56 -0600

    created a "country model"

--- Restaurant_manager/restaurant_manager/Menu/models.py
+++ Restaurant_manager/restaurant_manager/Menu/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Menu_items(models.Model):
     dish_name = (models.CharField(max_length=30))
+
+class Country(models.Model):
+    country_name = (models.CharField(max_length=25))
+    dish_name = models.ForeignKey(Menu_items, on_delete= models.CASCADE, related_name='Country')
+    
