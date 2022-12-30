c609106fc4359754723b846d2634ba24473c31a3
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2022-12-29 18:39:44 -0600

    added the models to admin

--- Restaurant_manager/restaurant_manager/Menu/admin.py
+++ Restaurant_manager/restaurant_manager/Menu/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Menu_items
+from .models import Menu_items, Country, Ingredients
 
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(Ingredients)
 admin.site.register(Menu_items)
