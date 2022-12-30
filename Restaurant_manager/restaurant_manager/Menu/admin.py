5545d5b821cc7c6d7c1d76f528cb9a19ac174faa
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2022-12-30 15:10:02 -0600

    because

--- Restaurant_manager/restaurant_manager/Menu/admin.py
+++ Restaurant_manager/restaurant_manager/Menu/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Menu_items, Country, Ingredients
 
-# Register your models here.
+# Register your models here. 
 admin.site.register(Country)
 admin.site.register(Ingredients)
 admin.site.register(Menu_items)
+# 
