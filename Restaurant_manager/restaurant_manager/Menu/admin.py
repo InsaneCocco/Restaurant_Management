1c65128a1df7ffbe61ad196b2538ecab4498dcf8
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2023-01-02 20:23:53 -0600

    created a model

--- Restaurant_manager/restaurant_manager/Menu/admin.py
+++ Restaurant_manager/restaurant_manager/Menu/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Menu_items, Country, Ingredients
+from .models import Menu_items, Country, Ingredients, Caracteristict
 
 # Register your models here. 
 admin.site.register(Country)
 admin.site.register(Ingredients)
 admin.site.register(Menu_items)
+admin.site.register(Caracteristict)
--- Restaurant_manager/restaurant_manager/Menu/models.py
+++ Restaurant_manager/restaurant_manager/Menu/models.py
@@ -16,6 +16,9 @@ class Menu_items(models.Model):
     country = (models.ForeignKey(Country, on_delete= models.CASCADE, related_name= 'Menu'))
     price = (models.IntegerField(default= 100))
 
+class Caracteristict(models.Model):
+    dish_name = (models.ForeignKey(Menu_items, on_delete=models.CASCADE, related_name='Caracteristict'))
+    # este modelo va a mostrar una tabla de cada una de las caracteristicas de los platillos. probablemente se tenga que cambiar a una dinamica porque va a haber muchos platillos
 
     # practicar como agregar datos a las tablas y despues obtener los datos de la shell
     # practicar como hacer merge y pull request para poder hacer commits en git
