82f59f879f2eb2d89bd100025beeb0317b66a43a
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2023-01-06 13:21:31 -0600

    renamed the caracteristics model to characteristics

--- Restaurant_manager/restaurant_manager/Menu/admin.py
+++ Restaurant_manager/restaurant_manager/Menu/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Menu_items, Country, Ingredients, Caracteristict
+from .models import Menu_items, Country, Ingredients, Characteristict
 
 # Register your models here. 
 admin.site.register(Country)
 admin.site.register(Ingredients)
 admin.site.register(Menu_items)
-admin.site.register(Caracteristict)
+admin.site.register(Characteristict)
