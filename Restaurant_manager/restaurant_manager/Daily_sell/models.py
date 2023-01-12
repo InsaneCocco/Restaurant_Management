d36ac616f15924e4d65dae4868f2e4a3dab49751
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2023-01-11 19:12:57 -0600

    created a Waiter model in the Employees app and registered, used that model as a FK in the daily model

--- Restaurant_manager/restaurant_manager/Daily_sell/models.py
+++ Restaurant_manager/restaurant_manager/Daily_sell/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from unicodedata import name
 
+from Employees.models import Waiter
+
 class Daily_sellM(models.Model):
+    Table = models.IntegerField()
+    waiter = (models.ForeignKey(Waiter, on_delete= models.CASCADE, related_name= 'waiter'))
     Dish = models.CharField(max_length=50)
     Quantity = models.IntegerField()
-    Table = models.IntegerField()
+    
+    
 
