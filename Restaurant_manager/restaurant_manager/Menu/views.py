03cf83f45ae73237990119c0d17f61a563eced4e
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2023-01-17 14:35:16 -0600

    feat[menu.views]:created a template and changed the httpresponse to a render function

--- Restaurant_manager/restaurant_manager/Menu/views.py
+++ Restaurant_manager/restaurant_manager/Menu/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 
 def menu_disp(request):
-    return HttpResponse(""" En esta pantalla se va a mostrar el menu cumpleto, donde venga el nombre, precio y de donde es el platillo.
-    tambien tiene que ser dinamico y si le picas a un platillo te muestra las caracteristicas de cada platillo
-    """)
+    about_content = { 'about' : " En esta pantalla se va a mostrar el menu cumpleto, donde venga el nombre, precio y de donde es el platillo. tambien tiene que ser dinamico y si le picas a un platillo te muestra las caracteristicas de cada platillo"}
+    return render(request, 'menu.html', about_content)
 
