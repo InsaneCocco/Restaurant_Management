c7d147b2de809212c78c64a156207bc8f274f0b2
Juan Pablo Cocco alcala <cocco@Juans-MacBook-Air.local>
2023-01-25 10:06:31 -0600

    fix[views]: elimine una view queestaba comentada porque ya estabareemplazada

--- Restaurant_manager/restaurant_manager/Menu/views.py
+++ Restaurant_manager/restaurant_manager/Menu/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Menu_items
 
-# def menu_disp(request):
-#     about_content = { 'about' : " En esta pantalla se va a mostrar el menu cumpleto, donde venga el nombre, precio y de donde es el platillo. tambien tiene que ser dinamico y si le picas a un platillo te muestra las caracteristicas de cada platillo"}
-#     return render(request, 'menu.html', about_content)
-
 
 def menu_disp(request):
     newmenu = Menu_items.objects.all()
